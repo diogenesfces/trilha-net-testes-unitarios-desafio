b164bd5df14c0ad1614f84320902b4a9e9514a14
Rodrigo de Souza Machado <107262755+Cwbmachado@users.noreply.github.com>
2024-06-02 11:38:18 -0300

    Update ValidacoesStringTests.cs
    Corrigindo anotação Fact

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -49,7 +49,7 @@ public class ValidacoesStringTests
       Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
+   [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
         // Arrange
